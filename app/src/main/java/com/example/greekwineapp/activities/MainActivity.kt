baf45cdbf0a443a764dc6fa9f35c0ef4471ecf3b
Denis Smithson <smithson11001@gmail.com>
2023-01-14 02:09:55 +0300

    #1.1 Adding new activity for testing features, basic design

--- app/src/main/java/com/example/greekwineapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/greekwineapp/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.greekwineapp
+package com.example.greekwineapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.greekwineapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
